90ff64a37d2875b38ade6a0c243ad170a96bb0ec
Jacob <jayjayb100@msn.com>
2020-09-15 20:03:52 -0500

    HOLY SHIT THIS MIGHT WORK

--- src/app/App.js
+++ src/app/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import {Col, Row} from "react-flexbox-grid";
 import CtaBox from "../main/components/CtaBox/CtaBox";
 import { bounceInRight } from 'react-animations'
+import Background from '../bg-img.png';
 let num = 5
+let style = {
+    backgroundImage: `url(${Background})`
+}
 
 function App() {
 
     return (
-        <div className="App">
+        <div className="App" style={style}>
             <div className="MainBox">
                 <Row middle="xs" center="xs" around="xs" className="TopBox">
                     <Col xs={num} className="quadrant-left" >
