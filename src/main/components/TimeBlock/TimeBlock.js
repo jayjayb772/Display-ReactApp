76ef9db196522f9d38b0a58e97aeb98eacbd5feb
Jacob <jayjayb100@msn.com>
2020-09-17 19:54:08 -0500

    Added time block on the bottom! and now it works like a clock

--- src/main/components/TimeBlock/TimeBlock.js
+++ src/main/components/TimeBlock/TimeBlock.js
@@ -4,15 +4,25 @@ import 'moment-timezone';
 import "./TimeBlock.css"
 
 function TimeBlock(props){
+    let [curTime, setCurTime] = useState(Date().toLocaleString())
+
+    useEffect(() => {
+        function update() {
+            setCurTime(Date().toLocaleString())
+        };
+        update();
+        const interval = setInterval(() => {
+            update()
+        }, 1000);
+        return () => clearInterval(interval);
+
+    }, [])
+
     return (
         <div>
-        <Moment format="h:mma" interval={1000} tz="America/Chicago">
-            {Date().toLocaleString()}
-        </Moment>
+        <Moment format="h:mm:ssa" interval={1000} tz="America/Chicago" date={curTime}/>
             <br/>
-            <Moment format="MMMM Do YYYY" interval={1000} tz="America/Chicago">
-                {Date().toLocaleString()}
-            </Moment>
+            <Moment format="MMMM Do YYYY" interval={1000} tz="America/Chicago" date={curTime}/>
         </div>
     )
 }
