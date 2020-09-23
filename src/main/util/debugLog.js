7659ad8f968f717b985242de1ec1d9906d02d341
Jacob <jayjayb100@msn.com>
2020-09-23 11:40:33 -0500

    adjusted debug

--- src/main/util/debugLog.js
+++ src/main/util/debugLog.js
@@ -1,5 +1,5 @@
 function debugLog(log, error){
-    if(process.env.REACT_APP_DEBUG){
+    if(process.env.REACT_APP_DEBUG === true){
         if(!error) {
             console.log(`DEBUG_LOG: ${log}`)
         }else{
