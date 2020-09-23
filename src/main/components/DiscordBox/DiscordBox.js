5a7e5c780466707864b1342971c8242eca848209
Jacob <jayjayb100@msn.com>
2020-09-22 23:05:42 -0500

    better reload

--- src/main/components/DiscordBox/DiscordBox.js
+++ src/main/components/DiscordBox/DiscordBox.js
@@ -6,54 +6,66 @@ import './DiscordBox.css';
 function DiscordBox(props){
     let [messages,setMessages] = useState([])
     const [isLoaded, setIsLoaded] = useState(false);
-    let url = `${process.env.REACT_APP_SOCKJS}`;
+    let url = `http://localhost:8080/websocket`;
     let sock = new SockJS(url);
+    let [isConnected, setIsConnected] = useState(false);
 
 
 useEffect(()=>{
-    let recInterval = null;
-    sock.onopen = async function() {
-        clearInterval(recInterval)
-        console.log('open');
-        console.log("OPENED CONNECTION")
-        sock.send('test');
-    };
+
     let new_conn = function(){
         console.log("opening new connection")
         sock =  new SockJS(url);
+        console.log("hello 6")
+
+        sock.onopen = function() {
+            console.log('open');
+            console.log("OPENED CONNECTION")
+            sock.send('test');
+            isConnected = true;
+            setIsConnected(true);
+            console.log("hello 5")
+        };
+
+        sock.onmessage = function(e) {
+            if(e.data === "test"){
+                console.log("hello 4")
+            }else {
+                console.log("hello 3")
+                let data = e.data
+                console.log('received message')
+                try {
+                    messages.push(JSON.parse(data))
+                    if (messages.length > 5) {
+                        console.log('messages greater than 4')
+                        messages.shift()
+                    }
+                    setMessages(messages)
+                    setIsLoaded(false)
+                    setIsLoaded(true)
+                }catch(err){
+                    console.log('Stay alive message!!');
+                }
+            }
+        };
+        sock.onclose = function() {
+            isConnected = false;
+            setIsConnected(false)
+            console.log("hello 2")
+        };
     }
 
-    sock.onmessage = async function(e) {
-        if(e.data === "test"){
 
-        }else {
-            let data = e.data
-            console.log('received message')
-            try {
-                messages.push(JSON.parse(data))
-                if (messages.length > 5) {
-                    console.log('messages greater than 4')
-                    messages.shift()
-                }
-                setMessages(messages)
-                setIsLoaded(false)
-                setIsLoaded(true)
-            }catch(err){
-                console.log('Stay alive message!!');
-            }
-        }
-    };
-    sock.onclose = function() {
-
-        recInterval = setInterval(function () {
-            new_conn();
-        }, 20000);
-    };
 
     const interval = setInterval(() => {
+        if(!isConnected){
+            console.log(isConnected)
+            new_conn()
+            console.log("hello 1")
+        }
         //sock.send('alive')
-        //console.log('Stay alive')
-    }, 100000);
+        console.log('Stay alive')
+    }, 10000);
     return () => clearInterval(interval);
 }, []);
 if(isLoaded){
--- src/main/components/DiscordBox/message/Message.js
+++ src/main/components/DiscordBox/message/Message.js
@@ -10,8 +10,8 @@ function Message(props){
                     - {props.from}
                     <div className="time">
                     <br/>
-                    <Moment date={props.ts} format="h:mm:ssa" tz="America/Chicago"/>
-                    <Moment date={props.ts} format="MMMM Do YYYY" tz="America/Chicago"/>
+                    <Moment unix parse={props.ts} format="h:mm:ssa" tz="America/Chicago"/>
+                    <Moment unix  parse={props.ts} format="MMMM Do YYYY" tz="America/Chicago"/>
                     </div>
                 </div>
             </div>
