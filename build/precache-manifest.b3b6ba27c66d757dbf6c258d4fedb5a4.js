26b42f24af438e04810ee916c83ebf9d52107337
Jacob <jayjayb100@msn.com>
2020-09-29 16:07:59 -0500

    new build

---
+++ build/precache-manifest.b3b6ba27c66d757dbf6c258d4fedb5a4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0d300c1a621f5091c30293a708bf15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae2d799e36090f85a839",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "817fc3aa9c2cffbc943b",
+    "url": "/static/css/main.54fbf276.chunk.css"
+  },
+  {
+    "revision": "ae2d799e36090f85a839",
+    "url": "/static/js/2.36724414.chunk.js"
+  },
+  {
+    "revision": "64c5f23e8fb6fd43132c563c35134885",
+    "url": "/static/js/2.36724414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817fc3aa9c2cffbc943b",
+    "url": "/static/js/main.53afcfbc.chunk.js"
+  },
+  {
+    "revision": "9fe3687da5704872eaf7",
+    "url": "/static/js/runtime-main.352792a7.js"
+  },
+  {
+    "revision": "5a9fa5747f4091b348e412c615b040a5",
+    "url": "/static/media/bg-img.5a9fa574.jpg"
+  },
+  {
+    "revision": "f373d592d59a2359328142eba74c9c25",
+    "url": "/static/media/bg-img.f373d592.png"
+  },
+  {
+    "revision": "26f83500f87f19e421a4f4e2bd6c7ae3",
+    "url": "/static/media/clear.26f83500.gif"
+  },
+  {
+    "revision": "866a18139e831c71ecbedd889f6fe378",
+    "url": "/static/media/cloudy.866a1813.gif"
+  },
+  {
+    "revision": "d60f18490d72308195e8e241e3979fc8",
+    "url": "/static/media/rain.d60f1849.gif"
+  },
+  {
+    "revision": "43e6a11d4a750d6ba2bf838576aba1e1",
+    "url": "/static/media/snow.43e6a11d.gif"
+  }
+]);
